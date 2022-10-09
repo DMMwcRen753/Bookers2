45c9cbf625becdf998ac5271a485de116bd29b58
EC2 Default User <ec2-user@ip-172-31-26-42.ap-northeast-1.compute.internal>
2022-10-09 07:04:06 +0000

    [update]トップページ更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,18 @@
 class BooksController < ApplicationController
   def index
+    @books = book.page(params[:page])
   end
 
   def show
+    @books = book.find(params[:id])
   end
 
   def edit
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,24 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.page(params[:page])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
+  
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
