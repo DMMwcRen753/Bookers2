558e53d94f1707ffe532979a3562607d3fb48034
EC2 Default User <ec2-user@ip-172-31-26-42.ap-northeast-1.compute.internal>
2022-10-15 14:50:05 +0000

    [finish]bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     if current_user
-      flash[:notice] = "Signed in successfully."
+      flash[:notice] = "Welcome! You have signed up successfully."
       user_path(current_user.id)
     else
       flash[:notice] = "Not Signed in successfully."
