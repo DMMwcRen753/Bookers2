4a1e71cb3762253edd93043610a1a315f1dc13c4
EC2 Default User <ec2-user@ip-172-31-26-42.ap-northeast-1.compute.internal>
2022-10-24 06:51:22 +0000

    [add]いいね機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-before_action :correct_user, only: [:edit, :update]
+before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
     @book = Book.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  
-  resources :books, only: [:create, :show, :index, :edit, :destroy, :update]
-  resources :users, only: [:show, :edit, :index, :update]
   get 'home/about' => 'homes#about', as: 'about'
+  resources :books, only: [:create, :show, :index, :edit, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :index, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
